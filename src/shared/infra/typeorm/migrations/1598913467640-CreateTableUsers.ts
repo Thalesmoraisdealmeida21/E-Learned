7aab5a2d64c59b4db2b3d3526e33e49bc6bf4d96
Thales Morais de Almeida <thales.morais21@gmail.com>
2020-11-17 19:50:08 -0300

    adicionado extension uuid nas migraçoes

--- src/shared/infra/typeorm/migrations/1598913467640-CreateTableUsers.ts
+++ src/shared/infra/typeorm/migrations/1598913467640-CreateTableUsers.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateTableUsers1598913467640
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'users',
